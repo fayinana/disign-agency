b0b2fc57bd08090c89515af56b268be7cd292407
fayinana <fekedeananiya@gmail.com>
2024-07-23 12:31:24 +0300

    add role for html and add animation for some sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,9 @@ const moreArrow = document.querySelector(".more-arrow");
 const jsArrow = document.querySelector(".js-arrow");
 const navbar = document.querySelector(".navbar");
 const searchBox = document.querySelector(".search-box .bx-search");
-
 const dateDOM = document.querySelector("#date");
 const date = new Date();
 dateDOM.innerHTML = date.getFullYear();
-
 searchBox.addEventListener("click", () => {
   navbar.classList.toggle("showInput");
   if (navbar.classList.contains("showInput")) {
@@ -20,7 +18,6 @@ searchBox.addEventListener("click", () => {
   }
 });
 // sidebar open close js code
-
 menuOpenBtn.onclick = function () {
   navLinks.style.left = "0";
 };
@@ -37,3 +34,48 @@ moreArrow.onclick = function () {
 jsArrow.onclick = function () {
   navLinks.classList.toggle("show3");
 };
+// courosel
+const pricings = document.querySelectorAll(".pricing");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const slides = document.querySelectorAll(".slide");
+const body = document.querySelector("body");
+if (body.clientWidth < 768) {
+  pricings.forEach((pricing, index) => {
+    pricing.style.left = `${index * 100}%`;
+  });
+  let counter = 0;
+  nextBtn.addEventListener("click", () => {
+    counter++;
+    carousel();
+  });
+  prevBtn.addEventListener("click", () => {
+    counter--;
+    carousel();
+  });
+  slides.forEach((slide, index) => {
+    slide.addEventListener("click", () => {
+      counter = index;
+      carousel();
+    });
+  });
+  function carousel() {
+    if (counter === pricings.length) {
+      counter = 0;
+    }
+    if (counter < 0) {
+      counter = pricings.length - 1;
+    }
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+      slides.forEach((s, i) => {
+        if (counter === i) {
+          s.classList.add("active");
+        }
+      });
+    });
+    pricings.forEach((pricing) => {
+      pricing.style.transform = `translate(-${counter * 100}%)`;
+    });
+  }
+}
